2b774b4abeee182bf432a8973fe4d202e3f9fa27
your name <gua@qiuweihaode-MacBook-Air.local>
2020-01-22 13:39:25 +0800

    version_1

---
+++ member_update.php
@@ -0,0 +1,228 @@
+<?php
+function GetSQLValueString($theValue, $theType) {
+  switch ($theType) {
+    case "string":
+      $theValue = ($theValue != "") ? filter_var($theValue, FILTER_SANITIZE_MAGIC_QUOTES) : "";
+      break;
+    case "int":
+      $theValue = ($theValue != "") ? filter_var($theValue, FILTER_SANITIZE_NUMBER_INT) : "";
+      break;
+    case "email":
+      $theValue = ($theValue != "") ? filter_var($theValue, FILTER_VALIDATE_EMAIL) : "";
+      break;
+    //case "url":
+    //  $theValue = ($theValue != "") ? filter_var($theValue, FILTER_VALIDATE_URL) : "";
+    //  break;      
+  }
+  return $theValue;
+}
+require_once("connMysql.php");
+session_start();
+//檢查是否經過登入
+if(!isset($_SESSION["loginMember"]) || ($_SESSION["loginMember"]=="")){
+	header("Location: index.php");
+}
+//執行登出動作
+if(isset($_GET["logout"]) && ($_GET["logout"]=="true")){
+	unset($_SESSION["loginMember"]);
+	unset($_SESSION["memberLevel"]);
+	header("Location: index.php");
+}
+//重新導向頁面
+$redirectUrl="member_center.php";
+//執行更新動作
+if(isset($_POST["action"])&&($_POST["action"]=="update")){	
+	$query_update = "UPDATE memberdata SET m_passwd=?, m_name=?, m_sex=?, m_birthday=?, m_email=?, m_url=?, m_phone=?, m_address=? WHERE m_id=?";
+	$stmt = $db_link->prepare($query_update);
+	//檢查是否有修改密碼
+	$mpass = $_POST["m_passwdo"];
+	if(($_POST["m_passwd"]!="")&&($_POST["m_passwd"]==$_POST["m_passwdrecheck"])){
+		$mpass = password_hash($_POST["m_passwd"], PASSWORD_DEFAULT);
+	}
+	$stmt->bind_param("ssssssssi", 
+		$mpass,
+		GetSQLValueString($_POST["m_name"], 'string'),
+		GetSQLValueString($_POST["m_sex"], 'string'),		
+		GetSQLValueString($_POST["m_birthday"], 'string'),
+		GetSQLValueString($_POST["m_email"], 'email'),
+		GetSQLValueString($_POST["m_url"], 'url'),
+		GetSQLValueString($_POST["m_phone"], 'string'),
+		GetSQLValueString($_POST["m_address"], 'string'),		
+		GetSQLValueString($_POST["m_id"], 'int'));
+	$stmt->execute();
+	$stmt->close();
+	//若有修改密碼，則登出回到首頁。
+	if(($_POST["m_passwd"]!="")&&($_POST["m_passwd"]==$_POST["m_passwdrecheck"])){
+		unset($_SESSION["loginMember"]);
+		unset($_SESSION["memberLevel"]);
+		$redirectUrl="index.php";
+	}		
+	//重新導向
+	header("Location: $redirectUrl");
+}
+
+//繫結登入會員資料
+$query_RecMember = "SELECT * FROM memberdata WHERE m_username='{$_SESSION["loginMember"]}'";
+$RecMember = $db_link->query($query_RecMember);	
+$row_RecMember = $RecMember->fetch_assoc();
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>網站會員系統</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+<script language="javascript">
+function checkForm(){
+	if(document.formJoin.m_passwd.value!="" || document.formJoin.m_passwdrecheck.value!=""){
+		if(!check_passwd(document.formJoin.m_passwd.value,document.formJoin.m_passwdrecheck.value)){
+			document.formJoin.m_passwd.focus();
+			return false;
+		}
+	}	
+	if(document.formJoin.m_name.value==""){
+		alert("請填寫姓名!");
+		document.formJoin.m_name.focus();
+		return false;
+	}
+	if(document.formJoin.m_birthday.value==""){
+		alert("請填寫生日!");
+		document.formJoin.m_birthday.focus();
+		return false;
+	}
+	if(document.formJoin.m_email.value==""){
+		alert("請填寫電子郵件!");
+		document.formJoin.m_email.focus();
+		return false;
+	}
+	if(!checkmail(document.formJoin.m_email)){
+		document.formJoin.m_email.focus();
+		return false;
+	}
+	return confirm('確定送出嗎？');
+}
+function check_passwd(pw1,pw2){
+	if(pw1==''){
+		alert("密碼不可以空白!");
+		return false;
+	}
+	for(var idx=0;idx<pw1.length;idx++){
+		if(pw1.charAt(idx) == ' ' || pw1.charAt(idx) == '\"'){
+			alert("密碼不可以含有空白或雙引號 !\n");
+			return false;
+		}
+		if(pw1.length<5 || pw1.length>10){
+			alert( "密碼長度只能5到10個字母 !\n" );
+			return false;
+		}
+		if(pw1!= pw2){
+			alert("密碼二次輸入不一樣,請重新輸入 !\n");
+			return false;
+		}
+	}
+	return true;
+}
+function checkmail(myEmail) {
+	var filter  = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	if(filter.test(myEmail.value)){
+		return true;
+	}
+	alert("電子郵件格式不正確");
+	return false;
+}
+</script>
+</head>
+
+<body>
+<table width="780" border="0" align="center" cellpadding="4" cellspacing="0">
+  <tr>
+    <td class="tdbline"><img src="images/mlogo.png" alt="會員系統" width="164" height="67"></td>
+  </tr>
+  <tr>
+    <td class="tdbline"><table width="100%" border="0" cellspacing="0" cellpadding="10">
+      <tr valign="top">
+        <td class="tdrline"><form action="" method="POST" name="formJoin" id="formJoin" onSubmit="return checkForm();">
+		<fieldset>
+          <legend class="title"><strong>登入資訊</strong></legend>
+          <div class="dataDiv">
+
+            <label><p>使用帳號：<?php echo $row_RecMember["m_username"];?></p></label>
+
+            <label><p>更改密碼：</label>
+            <input name="m_passwd" type="password" class="normalinput" id="m_passwd"placeholder="請填入新密碼">
+    		<input name="m_passwdo" type="hidden" id="m_passwdo" value="<?php echo $row_RecMember["m_passwd"];?>"></p>
+
+            <label><p>確認密碼：</label>
+            <input name="m_passwdrecheck" type="password" class="normalinput"id="m_passwdrecheck"placeholder="再填入一次"><br></p>
+			</fieldset>
+
+			<fieldset>
+            <legend class="heading"><strong>個人資訊</strong></legend>
+
+            <label><p>真實姓名：</label>
+            <input name="m_name" type="text" class="normalinput" id="m_name" value="<?php echo $row_RecMember["m_name"];?>"placeholder="請填入姓名">
+            <font color="#FF0000">*</font></p>
+
+			<label><p>大頭貼：
+			<input name="m_photo" type="file" class="normalinput" id="m_photo" value="<?php echo $row_RecMember["m_photo"];?>" >
+			</p></label>
+
+            <p>性　　別：
+            <label><input name="m_sex" type="radio" value="女" <?php if($row_RecMember["m_sex"]=="女") echo "checked";?>>女</label>
+    		<label><input name="m_sex" type="radio" value="男" <?php if($row_RecMember["m_sex"]=="男") echo "checked";?>>男</label>
+            <font color="#FF0000">*</font></p>
+
+            <label><p>生　　日：</label>
+            <input name="m_birthday" type="date" class="normalinput" id="m_birthday" value="<?php echo $row_RecMember["m_birthday"];?>">
+            <font color="#FF0000">*</font><br></p>
+
+            <label><p>電子郵件：</label>
+            <input name="m_email" type="email" class="normalinput" id="m_email" value="<?php echo $row_RecMember["m_email"];?>">
+            <font color="#FF0000">*</font><br></p>
+
+            <label><p>電　　話：</label>
+            <input name="m_phone" type="text" class="normalinput" id="m_phone" value="<?php echo $row_RecMember["m_phone"];?>"placeholder="沒有內容"></p>
+
+            <label><p>住　　址：</label>
+            <input name="m_address" type="text" class="normalinput" id="m_address" value="<?php echo $row_RecMember["m_address"];?>" size="40"placeholder="沒有內容"></p>
+
+			<label><p>個人簡介：
+            <textarea name="m_url" rows="10" cols="80" type="text" class="normalinput" id="m_url" value="<?php echo $row_RecMember["m_url"];?>" placeholder="沒有內容" ></textarea>
+			</p></label>
+
+            <p><font color="#FF0000">*</font> 表示為必填的欄位</p>
+          </div>
+          </fieldset>
+          <p align="center">
+            <input name="m_id" type="hidden" id="m_id" value="<?php echo $row_RecMember["m_id"];?>">
+            <input name="action" type="hidden" id="action" value="update">
+            <input type="submit" name="Submit2" value="修改資料">
+            <!-- <input type="reset" name="Submit3" value="重設資料"> -->
+            <input type="button" name="Submit" value="回上一頁" onClick="window.history.back();">
+          </p>
+        </form></td>
+        <td width="200">
+        <div class="boxtl"></div><div class="boxtr"></div>
+		<fieldset>
+        <div class="regbox">
+          <p align="center" class="heading"><strong>會員系統</strong></p>
+          
+            <p><strong><?php echo $row_RecMember["m_name"];?></strong> 您好。</p>
+            <p>您總共登入了 <?php echo $row_RecMember["m_login"];?> 次。<br>
+            本次登入的時間為：<br>
+            <?php echo $row_RecMember["m_logintime"];?></p>
+            <p align="center"><a href="member_center.php">會員中心</a> | <a href="?logout=true">登出系統</a></p>
+        </div>
+		</fieldset>	
+        <div class="boxbl"></div><div class="boxbr"></div></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="center" background="images/album_r2_c1.jpg" class="trademark">© 2020</td>
+  </tr>
+</table>
+</body>
+</html>
+<?php
+	$db_link->close();
+?>
